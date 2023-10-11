134bf2464dd731d950a6333495806d5c2a1dd21b
Sarah Leder <Trinity16201@gmail.com>
2023-10-10 21:53:41 -0400

    added weather details to current day card

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 //DONE-Connect to weather API
 //DONE-get city coordinates
-//get data from weather API
+//DONE-gget data from weather API
 //add data to display on current weather card
+//add icon to display
 //get data for five day weather
 //put city info search into local storage
 //pull city info into search history displayed on the page
@@ -10,22 +11,39 @@
 const searchButton = document.querySelector(".search-btn");
 const apiKey = "96f143ae7a73207f215000acd1e7e113";
 
+
+var weatherInfo = document.getElementById("weather-info");
+var currentDayIcon = document.getElementById("daily-icon");
 function currentWeather(data) {
+    console.log("current weather")
     var currentCity = data.city.name;
     var currentDate = data.list[0].dt_txt.split("")[0];
     var currentTemp = data.list[0].main.temp;
     var currentWind = data.list[0].wind.speed;
+    var currentHumidity = data.list[0].main.humidity;
+    var firstDayDetails = data.list[0].weather[0].description;
+    var firstDayIcon = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+
+    
+    var currentDayWeatherCard = document.createElement("p");
+    currentDayWeatherCard.innerHTML = `City:  ${currentCity} <br>` + `Date: ${currentDate}` + `Temp: ${Math.round(currentTemp)}` + `Wind: ${currentWind}` + `Humidity: ${currentHumidity}`;
+    weatherInfo.append(currentDayWeatherCard);
+
+    currentDayIcon.setAttribute("src", "https://openweathermap.org/img/wn/10d@4x.png")
+    currentDayIcon.innerHTML = `${firstDayDetails} + ${firstDayIcon}`
 }
 
-const getWeatherInfo = (name, lat, lon )=>{
-    var apiURL=`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}}&units=imperial`;
+const getWeatherInfo = (name, lat, lon) => {
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(apiURL)
-    .then((response) => response.json())
+        .then((response) => response.json())
         .then((data) => {
-            if (!data.length)
+            console.log("get weather info", data)
+            if (!data || !data.list)
                 return alert("Error-could not find the location you are looking for.")
-            console.log(data[0])
+            currentWeather(data);
         }).catch(function (error) {
+            console.log(error)
         });
 }
 var cityInput = document.querySelector(".city-input")
@@ -34,13 +52,13 @@ function getCoordinates() {
     var apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
 
     fetch(apiURL)
-    .then((response) => response.json())
+        .then((response) => response.json())
         .then((data) => {
             if (!data.length)
                 return alert("Error-try again")
             var { name, lat, lon } = data[0];
             getWeatherInfo(name, lat, lon)
-            console.log(data)
+
         }).catch(function (error) {
             console.log(error)
         });
